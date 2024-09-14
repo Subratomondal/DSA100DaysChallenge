ab96d4f136b52d24bcbdb9ce12e45798cd8c7b2b
Subrato Mondal <subratomondal374@gmail.com>
2024-09-14 20:53:35 +0530

    solving array using recursion

---
+++ isSortedArray.java
@@ -0,0 +1,15 @@
+class isSortedArray{
+    public static void main(String[] args) {
+        int[] arr ={5,6,7,8,9,10};
+        System.out.println(sorted(arr,0 ));
+    }
+
+    static boolean sorted(int[] arr, int index){
+        if (index==arr.length-1) {
+            return true;
+        }
+        return arr[index]<arr[index+1] && sorted(arr,index+1);
+    }
+
+
+}
